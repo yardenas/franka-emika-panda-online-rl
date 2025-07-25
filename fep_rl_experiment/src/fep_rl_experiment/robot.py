Fix quaternion

fep_rl_experiment/src/fep_rl_experiment/robot.py
@@ -66,7 +66,7 @@ class Robot:
                 [0.0000000e00, 0.0000000e00, 0.0000000e00, 1.0000000e00],
             ]
         )
-        self.goal_tip_quat = R.from_matrix(self.goal_tip_transform[:3, :3]).as_quat()
+        self.goal_tip_quat = np.array([0.0, 1.0, 0.0, 0.0])
         self.start_pos = np.array([6.6105318e-01, -5.1778345e-04, 1.7906836e-01])
         self.ee_velocity_estimator = LinearVelocityEstimator()
         self.tf_buffer = tf2_ros.Buffer()