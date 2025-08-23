b1cf09a0c7a8a4817a408cf82da7c26eba3ada96
Yarden As <yarden.as@inf.ethz.ch>
2025-08-23 13:28:54 +0200

    Fingers obs (#18)
    * Add fingers and action history
    
    * Normalize
    
    * Make slightly harder
    
    * Add logs

--- fep_rl_experiment/src/fep_rl_experiment/robot.py
+++ fep_rl_experiment/src/fep_rl_experiment/robot.py
@@ -201,7 +201,8 @@ class Robot:
         pose_msg.pose.orientation.w = float(self.goal_tip_quat[3])
         self._desired_ee_pose_pub.publish(pose_msg)
         fingers = self.joint_state[-2:].mean()
-        if action[3] >= 0.95:
+        if action[3] >= 0.:
+            rospy.loginfo("Opening gripper")
             goal = GripperCommandActionGoal()
             goal.header.stamp = rospy.Time.now()
             if fingers < 0.015:
@@ -215,6 +216,7 @@ class Robot:
             goal.goal.command.max_effort = 0.0
             self.gripper_command_pub.publish(goal)
         else:
+            rospy.loginfo("Closing Gripper")
             goal = GripperCommandActionGoal()
             goal.header.stamp = rospy.Time.now()
             goal.goal.command.position = np.clip(fingers - 0.01, 0.0, 0.0402)
